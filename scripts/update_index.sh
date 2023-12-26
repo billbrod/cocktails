bcd111f242c7d7599a5abc51df431c4e358f3e0c
William F. Broderick <billbrod@gmail.com>
2023-12-26 15:33:42 -0500

    get private cocktials working

--- scripts/update_index.sh
+++ scripts/update_index.sh
@@ -2,16 +2,16 @@
 set -euo pipefail
 
 sed -i '/{ #recent }/q' docs/cocktails/index.md
-echo "" >> docs/index.md
-echo "|Cocktail| Recency (lower is newer) { data-sort-method='number' } |" >> docs/index.md
-echo "|------|-----------|" >> docs/index.md
-all_files="$(ls docs/cocktails/*md)"
+echo "" >> docs/cocktails/index.md
+echo "|Cocktail| Recency (lower is newer) { data-sort-method='number' } |" >> docs/cocktails/index.md
+echo "|------|-----------|" >> docs/cocktails/index.md
+all_files="$(ls $1/*md)"
 for file in ${all_files[*]}; do
     if [[ $file != *"index.md" ]]; then
         recipe_name="$(grep -h '^# ' $file)"
         # capture only the number
         order="$(grep -Po '(?<=order: )([0-9]+)' $file)"
-        file=${file/docs\///}
-        echo "|[${recipe_name/\# /}](${file/.md//})|$order|" >> docs/index.md
+        file=${file/\/\//\//}
+        echo "|[${recipe_name/\# /}](${file/.md//})|$order|" >> docs/cocktails/index.md
     fi
 done
